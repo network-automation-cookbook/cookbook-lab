c74c56985d026d754409a32f0a423fdc3c2987aa
Jeff Kala <jeff.l.kala@gmail.com>
2024-12-02 06:21:24 +0000

    final few updates for ch3

--- chapters/ch03/filter_plugins/filter.py
+++ chapters/ch03/filter_plugins/filter.py
@@ -5,7 +5,17 @@ class FilterModule(object):
 
     def filters(self):
         """Default filters method to overload"""
-        return {"acl_state": self.acl_state}
+        return {
+            "acl_state": self.acl_state,
+            "custom_acl": self.custom_acl,
+            }
+
+    def custom_acl(self,acl_def,field=None):
+        for acl_name, acl_rules in acl_def.items():
+            for rule in acl_rules:
+                if field and field in rule.keys():
+                    rule[field] = rule[field].upper()
+        return acl_def
 
     def acl_state(self,acl_def):
         """ACL State filter logic."""
