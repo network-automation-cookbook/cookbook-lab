23162ede92bfcfd2bd4db759cfc51c55250f4fb6
Christian Adell <chadell@gmail.com>
2024-12-27 07:29:49 +0000

    population completed

--- chapters/ch12/roles/build_nautobot_db/lookup_plugins/calculate_subnets.py
+++ chapters/ch12/roles/build_nautobot_db/lookup_plugins/calculate_subnets.py
@@ -1,10 +1,6 @@
 from ansible.errors import AnsibleError, AnsibleParserError
 from ansible.plugins.lookup import LookupBase
-from ansible.utils.display import Display
 import netaddr
-
-
-display = Display()
 
 class LookupModule(LookupBase):
     """
@@ -12,17 +8,10 @@ class LookupModule(LookupBase):
     Example usage:
         "{{ lookup('calculate_subnets', '192.168.0.0/24', 26) }}"
     """
-
     def run(self, terms, variables=None, **kwargs):
-        # Ensure we have two terms: the network prefix and the subnet size
-        if len(terms) != 2:
-            raise AnsibleError("The 'validate_subnets' lookup requires two arguments: network and subnet size")
-
         network = terms[0]
         subnet_size = int(terms[1])
-
         try:
-            # Use netaddr to calculate subnets
             ip_network = netaddr.IPNetwork(network)
             subnets = list(ip_network.subnet(subnet_size))
             return [str(subnet) for subnet in subnets]
